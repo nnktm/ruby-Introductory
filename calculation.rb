b1266ad225f1a9ae1068862e7a0381cd8091feaa
nnktm <s1f102402417@iniad.org>
2026-04-21 11:26:15 +0900

    基本の計算や変数について

---
+++ calculation.rb
@@ -0,0 +1,8 @@
+puts 37 #数値
+puts 2 + 9 #足し算
+puts 2 - 9 #引き算
+puts 13 * 9 #掛け算
+puts 32 / 8 #割り算
+puts 18 % 5 #割り算の余り
+
+puts "Hello" + "World" #文字列の結合
---
+++ variable.rb
@@ -0,0 +1,9 @@
+name = "Bob"
+age = 20
+hobby = "soccer"
+
+puts name
+puts age
+puts hobby
+
+puts name + " is " + age.to_s + " years old and likes " + hobby + "."
