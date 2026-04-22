7e982c809be5a0b7c6cc4dcf685aa2791ccc28fa
nnktm <s1f102402417@iniad.org>
2026-04-22 23:49:18 +0900

    辞書と繰り返し

---
+++ array.rb
@@ -0,0 +1,14 @@
+array = [1, 2, 3, 4, 5]
+puts array[0]
+puts array[1]
+puts array[2]
+puts array[3]
+puts array[4]
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+puts fruits[0]
+puts fruits[1]
+puts fruits[2]
+puts fruits[3]
+puts fruits[4]
+
---
+++ roop.rb
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+
+fruits.each do |fruit|
+    puts "I like " + fruit + "s."
+end
+
+for fruit in fruits
+    puts "I like " + fruit + "s."
+end
+
