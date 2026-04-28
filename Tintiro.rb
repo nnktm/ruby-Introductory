7339b1b83eb2594f7265bd18559752b5aef0ff13
nnktm <s1f102402417@iniad.org>
2026-04-28 11:08:05 +0900

    チンチロゲーム作成part2

--- Tintiro.rb
+++ Tintiro.rb
@@ -15,27 +15,71 @@ def role(x, y, z)
     end
 end
 
+def Win_or_Lose(cpu_result, user_result)
+    cpu_score = case cpu_result
+    when "ピンゾロ" then 600
+    when "ゾロ目" then 500
+    when "シゴロ" then 400
+    when "目なし" then 200
+    when "ヒフミ" then 100
+    when /\A([1-6])の目\z/ then 300 + ::Regexp.last_match(1).to_i
+    else 0
+    end
+
+    user_score = case user_result
+    when "ピンゾロ" then 600
+    when "ゾロ目" then 500
+    when "シゴロ" then 400
+    when "目なし" then 200
+    when "ヒフミ" then 100
+    when /\A([1-6])の目\z/ then 300 + ::Regexp.last_match(1).to_i
+    else 0
+    end
+
+    return "引き分け" if cpu_score == user_score
+    cpu_score > user_score ? "CPUの勝ち" : "あなたの勝ち"
+end
+
 def play()
-    cpu_result = ""
-    user_result = ""
+    cpu_result = "目なし"
+    user_result = "目なし"
     for i in 1..3
         cpu_dice = Array.new(3) { rand(1..6) }
-        if role(cpu_dice[0], cpu_dice[1], cpu_dice[2]) !="目無し"
+        if role(cpu_dice[0], cpu_dice[1], cpu_dice[2]) != "目なし"
             cpu_result = role(cpu_dice[0], cpu_dice[1], cpu_dice[2])
             break
         else
-            cpu_result = "目無し"
+            cpu_result = "目なし"
         end
     end
     for i in 1..3
         user_dice = Array.new(3) { rand(1..6) }
-        if role(user_dice[0], user_dice[1], user_dice[2]) !="目無し"
+        if role(user_dice[0], user_dice[1], user_dice[2]) != "目なし"
             user_result = role(user_dice[0], user_dice[1], user_dice[2])
             break
         else
-            user_result = "目無し"
+            user_result = "目なし"
         end
-    
+    end
+    return cpu_result, user_result
 end
 
-play()
+def tintiro()
+    print " サイコロを振りますか？(y/n): "
+    answer = gets.chomp
+    if answer == "y"
+        cpu_result, user_result = play()
+        print"結果を見ますか？(y/n): "
+        answer = gets.chomp
+        if answer == "y"
+            result = Win_or_Lose(cpu_result, user_result)
+            puts "CPUの結果: #{cpu_result}"
+            puts "あなたの結果: #{user_result}"
+            puts "#{result}"
+        end
+    else
+        puts "ゲームを終了します。"
+    end
+end
+
+tintiro()
